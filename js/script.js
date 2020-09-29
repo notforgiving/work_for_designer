520b75ce46910c12db82774d97e2ed48604e74cb
Ivlevala Ludmila <62250565+notforgiving@users.noreply.github.com>
2020-09-29 13:28:42 +0300

    Поправлены шрифты. Сверстана мобильная версия главной страницы
    Поправлены размеры блоков, шрифтов, отступов.
    Мобильная версия  главного экрана сверстана

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-/*$(window).on('load', function () {
-    $preloader = $('.loaderArea'),
-    $loader = $preloader.find('.loader');
-    $loader.fadeOut();
-    $preloader.delay(2000).fadeOut('slow');
-  });*/
-
   jQuery(document).ready(function($) {
     $(window).on('load', function () {
       var $preloader = $('.loaderArea'),
@@ -13,3 +6,19 @@
       $preloader.delay(250).fadeOut(200);
     });
   });
+
+  $('.menu-btn').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('menu-btn_active');
+    $('.mobil_menu').toggleClass('mobil_menu_active');
+  });
+
+  $(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+
+
+});
