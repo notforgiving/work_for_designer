a6b4b15c3dbba5f357e4c04ed96c4d0ec38ac0cf
Self_C <not_forgiving@list.ru>
2020-10-11 19:37:15 +0300

    change mobil menu style

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 /*Обработчик прелодера*/
-$(window).on('load', function () {
-  $preloader = $('.loaderArea'),
-  $loader = $preloader.find('.loader');
-  $loader.delay(550).fadeOut('slow');
-  $preloader.delay(550).fadeOut('slow');
-  $( ".loader" ).delay(650).animate({
+$(window).on("load", function () {
+  ($preloader = $(".loaderArea")), ($loader = $preloader.find(".loader"));
+  $loader.delay(550).fadeOut("slow");
+  $preloader.delay(550).fadeOut("slow");
+  $(".loader").delay(650).animate({
     display: "block",
-    opacity: "1"
+    opacity: "1",
   });
-  $('.loader').delay(650).attr('style','display:block;')
+  $(".loader").delay(650).attr("style", "display:block;");
 });
 
 /*Обработчик меню мобильного показать/скрыть*/
-  $('.menu-btn').on('click', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('menu-btn_active');
-    $('.mobil_menu').toggleClass('mobil_menu_active');
+$(".menu-btn").on("click", function (e) {
+  e.preventDefault;
+  $(this).toggleClass("menu-btn_active");
+  $(".mobil_menu").toggleClass("mobil_menu_active");
+});
+
+/*Обработчик плавной отправки при нажатии на контакты*/
+$(function () {
+  $("a[href^='#']").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
   });
 
-  /*Обработчик плавной отправки при нажатии на контакты*/
-  $(function(){
-    $("a[href^='#']").click(function(){
-            var _href = $(this).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-            return false;
-    });
-
-    /*Инициализация срабатывания анимации при скроле*/
-    new WOW().init();
+  /*Инициализация срабатывания анимации при скроле*/
+  new WOW().init();
 });
